ebbc75267a5e422450d63bbfdd8798de3c11e657
JusticeJudy <charliu.duke@gmail.com>
2016-03-18 17:38:14 -0500

    Add file entry (done)

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -57,8 +57,8 @@
 						@endif
 				</div>
 
-				<div class="form-group fileupload" id="fileupload">
-						{{ Form::file('floorplan') }}
+				<div class="form-group fileupload">
+						{{ Form::file('floorplan[]', ['multiple']) }}
 						@if($errors->has('floorplan'))
 							{{ $errors->first('floorplan')  }}
 						@endif
@@ -91,7 +91,7 @@
 	<script>
 		$(document).ready(function(){
 
-			var item= $("#fileupload").clone(true,true);
+			
 			// var item='<div class="form-group fileupload">{{ Form::file("floorplan") }}@if($errors->has("floorplan")){{ $errors->first("floorplan")  }}@endif<span class="hide view">View</span><span class="hide delete">Delete</span><span class="hide add">Add Another File</span></div>';                                                                                                                                                                              
 				$('input[type=file]').change(function(){
 			// $(".fileupload").on("change",('input[type=file]'),  function(){
@@ -100,7 +100,8 @@
 				var reader= new FileReader();
 				reader.readAsDataURL(file);
 				$("span").removeClass("hide");
-				});
+
+				
 				
 				// $("iframe").attr("src", URL.createObjectURL(file));
 
@@ -122,16 +123,18 @@
 
 				$('.add').click(function(){
 				// $(".fileupload").on("click",".add", function(event){
-					 var fileinput = $(this).parent();
+					 var $fileinput = $(this).parent();
 					
 					$(this).remove();
-					console.log(fileinput);
-					$(fileinput).after("<br>").after(item);
+					console.log($fileinput);
+					$(item).clone(true, true).insertAfter($fileinput);
+					// $(fileinput).after(item).clone(true,true).after("<br>");
 					
 					console.log(item);
 				});
+				});
 
-				
+				var item= $(".fileupload").clone(true,true);
 				// $(reader).load(function(e){
 				// 	console.log("reader");
 				// 	$("img").attr("src", e.target.result);
